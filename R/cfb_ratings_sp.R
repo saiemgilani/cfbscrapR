73e29192a521cd51155ab6a46fcd52c1506f4ed9
saiemgilani <saiem.gilani@gmail.com>
2020-09-01 16:17:37 -0400

    Update cfb_ratings_sp.R
    rename columns

--- R/cfb_ratings_sp.R
+++ R/cfb_ratings_sp.R
@@ -55,7 +55,30 @@ cfb_ratings_sp <- function(year = NULL, team = NULL){
   check_status(res)
 
   # Get the content and return it as data.frame
-  df = fromJSON(full_url)
+  df = fromJSON(full_url, flatten = TRUE) %>% 
+    rename(
+      second_order_wins = .data$secondOrderWins,
+      offense_rating = .data$offense.rating,
+      offense_success = .data$offense.success,
+      offense_explosiveness = .data$offense.explosiveness,
+      offense_rushing = .data$offense.rushing,
+      offense_passing = .data$offense.passing,
+      offense_standard_downs = .data$offense.standardDowns,
+      offense_passing_downs = .data$offense.passingDowns,
+      offense_run_rate = .data$offense.runRate,
+      offense_pace = .data$offense.pace,
+      defense_rating = .data$defense.rating,
+      defense_success = .data$defense.success,
+      defense_explosiveness = .data$defense.explosiveness,
+      defense_rushing = .data$defense.rushing,
+      defense_passing = .data$defense.passing,
+      defense_standard_downs = .data$defense.standardDowns,
+      defense_passing_downs = .data$defense.passingDowns,
+      defense_havoc_total = .data$defense.havoc.total,
+      defense_havoc_front_seven = .data$defense.havoc.frontSeven,
+      defense_havoc_db = .data$defense.havoc.db,
+      special_teams_rating = .data$specialTeams.rating) %>% 
+    as.data.frame()
 
   return(df)
 }
