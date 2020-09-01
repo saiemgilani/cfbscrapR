b06394a68d397a61532530830f130eb123a2632c
saiemgilani <saiem.gilani@gmail.com>
2020-09-01 10:38:36 -0400

    Create test-cfb_game_player_stats.R

---
+++ tests/testthat/test-cfb_game_player_stats.R
@@ -0,0 +1,13 @@
+library(testthat)
+library(cfbscrapR)
+
+test_that("CFB Game Player Stats", {
+
+  x <- cfb_game_player_stats(2018, week = 15, conference = 'Ind')
+  y <- cfb_game_player_stats(2013, week = 1, team = "Florida State", category = 'passing')
+
+  
+  expect_s3_class(x, "data.frame")
+  expect_s3_class(y, "data.frame")
+
+})
