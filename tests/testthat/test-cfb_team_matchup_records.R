3e43a95cb33d2693cfb51bbe6b35bc349b790b68
saiemgilani <saiem.gilani@gmail.com>
2020-09-01 14:37:38 -0400

    Create test-cfb_team_matchup_records.R

---
+++ tests/testthat/test-cfb_team_matchup_records.R
@@ -0,0 +1,16 @@
+library(testthat)
+library(cfbscrapR)
+
+test_that("CFB Team Matchup Records", {
+  x <- cfb_team_matchup_records('Texas','Oklahoma')
+  y <- cfb_team_matchup_records('Texas A&M','TCU', min_year = 1975)
+  
+  cols <- c('start_year', 'end_year', 'team1', 'team1_wins',   'team2', 'team2_wins', 'ties')
+  
+  
+  expect_equal(colnames(x), cols)
+  expect_equal(colnames(y), cols)
+  expect_s3_class(x, "data.frame")
+  expect_s3_class(y, "data.frame")
+  
+})
