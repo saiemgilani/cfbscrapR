9c026c8b5d3f2cf5c5c315aaff865841a53b935b
saiemgilani <saiem.gilani@gmail.com>
2020-09-04 02:00:53 -0400

    Update test-cfb_game_media.R

--- tests/testthat/test-cfb_game_media.R
+++ tests/testthat/test-cfb_game_media.R
@@ -9,11 +9,12 @@ test_that("CFB Game Media", {
   y <- cfb_game_media(2018, week = 4, conference = 'Ind')
 
   
-  # cols <- c("game_id", "season", "season_type", "week", 
-  #           "home_team", "home_conference", "home_score",
-  #           "away_team", "away_conference", "away_score", 
-  #           "provider","spread", "formatted_spread", "over_under")
+  cols <- c("game_id", "season", "week", "season_type", "start_time",
+            "is_start_time_tbd", "home_team", "home_conference", "away_team",
+            "away_conference","tv", "radio", "web")
   
+  expect_equal(colnames(x), cols)
+  expect_equal(colnames(y), cols)
   expect_s3_class(x, "data.frame")
   expect_s3_class(y, "data.frame")
   
