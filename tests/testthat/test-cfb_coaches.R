62090cafef87ba396fc12fa0327c285c1bacd9ae
saiemgilani <saiem.gilani@gmail.com>
2020-09-13 08:13:55 -0400

    Update test-cfb_coaches.R

--- tests/testthat/test-cfb_coaches.R
+++ tests/testthat/test-cfb_coaches.R
@@ -3,7 +3,8 @@ context("CFB Coaches")
 x <- cfb_coaches(first = "Nick", last = "Saban", team = 'alabama')
 
 cols <- c('first_name','last_name','school','year', 'games',
-          'wins', 'losses', 'ties', 'preseason_rank', 'postseason_rank')
+          'wins', 'losses', 'ties', 'preseason_rank', 'postseason_rank','srs','sp_overall',
+          'sp_offense','sp_defense')
 
 test_that("CFB Coaches", {
   expect_equal(colnames(x), cols)
