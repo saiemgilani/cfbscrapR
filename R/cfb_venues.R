c2ad132a213b6bf9d6f9e7ae1c35282723f91c45
saiemgilani <saiem.gilani@gmail.com>
2020-09-22 22:06:01 -0400

    Update cfb_venues.R
    rename `id` to `venue_id`

--- R/cfb_venues.R
+++ R/cfb_venues.R
@@ -2,9 +2,28 @@
 #'
 #' Pulls all college football venues and data on capacity, grass, city/state, location,
 #' elevation, dome, timezone and construction year
+#' 
+#' @return A data frame with 335 rows and 13 variables:
+#' \describe{
+#'   \item{\code{venue_id}}{integer.}
+#'   \item{\code{name}}{character.}
+#'   \item{\code{capacity}}{integer.}
+#'   \item{\code{grass}}{logical.}
+#'   \item{\code{city}}{character.}
+#'   \item{\code{state}}{character.}
+#'   \item{\code{zip}}{character.}
+#'   \item{\code{country_code}}{character.}
+#'   \item{\code{location}}{list.}
+#'   \item{\code{elevation}}{character.}
+#'   \item{\code{year_constructed}}{integer.}
+#'   \item{\code{dome}}{logical.}
+#'   \item{\code{timezone}}{character.}
+#' }
+#' @source \url{https://api.collegefootballdata.com/venues}
 #' @keywords Venues
 #' @importFrom jsonlite "fromJSON"
 #' @importFrom httr "GET"
+#' @import dplyr
 #' @export
 #' @examples
 #'
@@ -26,7 +45,9 @@ cfb_venues <- function(){
   check_status(res)
 
   # Get the content and return it as data.frame
-  df = jsonlite::fromJSON(base_url)
+  df = jsonlite::fromJSON(base_url) %>% 
+    dplyr::rename(venue_id = .data$id) %>% 
+    as.data.frame()
 
   return(df)
 }
