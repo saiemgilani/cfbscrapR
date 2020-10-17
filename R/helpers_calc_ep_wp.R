e3cd6d1c559fedb74815a237ee5374434bea0ec9
saiemgilani <saiem.gilani@gmail.com>
2020-10-17 15:36:41 -0400

    Update helpers_calc_ep_wp.R

--- R/helpers_calc_ep_wp.R
+++ R/helpers_calc_ep_wp.R
@@ -1,6 +1,7 @@
 #' Select the columns needed for EP predictions
 #' @param pbp (\emph{data.frame} required) Play-by-Play dataframe pulled from API via the `cfb_pbp_data()` function
-#' @importFrom dplyr "select"
+#' @importFrom dplyr "select" 
+#' @importFrom tidyr "everything"
 ep_model_select <- function(pbp) {
   
   pbp <- pbp %>%
@@ -12,17 +13,63 @@ ep_model_select <- function(pbp) {
       "yards_to_goal",
       "log_ydstogo",
       "Goal_To_Go",
-      "Under_two"
+      "Under_two",
+      "pos_score_diff_start",
+      tidyr::everything()
     )
   return(pbp)
   
 }
 
-
+#' Select the columns needed for EP predictions
+#' @param pbp (\emph{data.frame} required) Play-by-Play dataframe pulled from API via the `cfb_pbp_data()` function
+#' @importFrom dplyr "select" 
+#' @importFrom tidyr "everything"
+ep_model_select_check <- function(pbp) {
+  
+  pbp <- pbp %>%
+    dplyr::select(
+      "pos_team",
+      "def_pos_team",
+      "half",
+      "period",
+      "TimeSecsRem",
+      "TimeSecsRem_end",
+      "lag_play_type",
+      "lead_play_type",
+      "play_type",
+      "play_text",
+      "yards_to_goal",
+      "yards_to_goal_end",
+      "down",
+      "down_end",
+      "distance",
+      "distance_end",
+      "yards_gained",
+      "change_of_pos_team",
+      "pos_score_diff_start",
+      "pos_score_diff_start_end",
+      "pos_score_diff",
+      "EPA",
+      "ep_before",
+      "ep_after",
+      "log_ydstogo",
+      "log_ydstogo_end",
+      "Goal_To_Go",
+      "Goal_To_Go_end",
+      "Under_two",
+      "Under_two_end",
+      tidyr::everything()
+    )
+  return(pbp)
+  
+}
 
 #' Select the columns needed for WP predictions
 #' @param pbp (\emph{data.frame} required) Play-by-Play dataframe pulled from API via the `cfb_pbp_data()` function
-#' @importFrom dplyr "select"
+#' @importFrom dplyr "select" 
+#' @importFrom tidyr "everything"
+
 wp_model_select <- function(pbp) {
   
   pbp <- pbp %>%
@@ -31,21 +78,27 @@ wp_model_select <- function(pbp) {
       "offense_play",
       "defense_play",
       "home",
+      "away",
+      "pos_team",
+      "def_pos_team",
+      "pos_score_diff",
+      "pos_score_diff_start",
+      "score_diff",
+      "score_diff_start",
       "half",
       "TimeSecsRem",
       "adj_TimeSecsRem",
       "ep_before",
-      "score_diff",
-      "score_diff_start",
       "ExpScoreDiff",
       "ExpScoreDiff_Time_Ratio",
       "down",
       "distance",
       "yards_to_goal",
-      "off_timeouts_rem_before",
-      "def_timeouts_rem_before",
+      "pos_team_timeouts_rem_before",
+      "def_pos_team_timeouts_rem_before",
       "play_type",
-      "play_text"
+      "play_text",
+      tidyr::everything()
     )
   
   return(pbp)
@@ -56,6 +109,7 @@ wp_model_select <- function(pbp) {
 #' Select the columns needed for WP predictions
 #' @param pbp (\emph{data.frame} required) Play-by-Play dataframe pulled from API via the `cfb_pbp_data()` function
 #' @importFrom dplyr "select"
+#' @importFrom tidyr "everything"
 wp_model_select_check <- function(pbp) {
   
   pbp <- pbp %>%
@@ -63,6 +117,11 @@ wp_model_select_check <- function(pbp) {
       "offense_receives_2H_kickoff",
       "half",
       "period",
+      "pos_team",
+      "def_pos_team",
+      
+      "pos_score_diff",
+      "pos_score_diff_start",
       "TimeSecsRem",
       "adj_TimeSecsRem",
       "ExpScoreDiff",
@@ -75,22 +134,24 @@ wp_model_select_check <- function(pbp) {
       "yards_to_goal",
       "half",
       "home",
-      "off_timeouts_rem_before",
-      "def_timeouts_rem_before",
+      "pos_team_timeouts_rem_before",
+      "def_pos_team_timeouts_rem_before",
       "play_type",
       "play_text",
       "wp_before",
       "wp_after",
       "wpa",
-      "wpa_change_ind",
-      "wpa_change",
-      "wpa_base_ind",
       "wpa_base",
+      "wpa_base_ind",
+      "wpa_change",
+      "wpa_change_ind",
       "wpa_base_nxt",
+      "wpa_base_nxt_ind",
       "wpa_change_nxt",
+      "wpa_change_nxt_ind",
       "lead_wp_before",
       "lead_wp_before2",
-      
+      tidyr::everything()
     )
   
   return(pbp)
