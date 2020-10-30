7ac93289f8cf49c79f393771de9c3f034f6a05ed
saiemgilani <saiem.gilani@gmail.com>
2020-10-30 19:43:28 -0400

    Update cfb_betting_lines.R

--- R/cfb_betting_lines.R
+++ R/cfb_betting_lines.R
@@ -162,10 +162,9 @@ cfb_betting_lines <- function(game_id = NULL,
           dplyr::rename(game_id = .data$id) %>% 
           as.data.frame()
       }
-      # message(glue::glue("{Sys.time()}: Scraping betting lines data..."))
+
     },
     error = function(e) {
-      # message(glue::glue("{Sys.time()}: Invalid arguments or no betting lines data available!"))
     },
     warning = function(w) {
     },
