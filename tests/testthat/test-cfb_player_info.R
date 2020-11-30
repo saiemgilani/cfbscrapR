510139ca9234dc7c86f349bd29b8e7c5092c045d
Saiem Gilani <saiem.gilani@gmail.com>
2020-11-30 03:59:40 -0500

    Update test-cfb_player_info.R

--- tests/testthat/test-cfb_player_info.R
+++ tests/testthat/test-cfb_player_info.R
@@ -4,8 +4,6 @@ x <- cfb_player_info(search_term = 'James', position = 'DB', team = 'Florida Sta
 
 y <- cfb_player_info(search_term = 'Lawrence', team = "Clemson")
 
-z <- cfb_player_info(search_term = 'Tua', position = 'QB')
-
 w <- cfb_player_info(search_term = 'Duggan')
 
 cols <- c("athlete_id","team","name","first_name","last_name",
@@ -15,10 +13,8 @@ cols <- c("athlete_id","team","name","first_name","last_name",
 test_that("CFB Player Info", {
   expect_equal(colnames(x), cols)
   expect_equal(colnames(y), cols)
-  expect_equal(colnames(z), cols)
   expect_equal(colnames(w), cols)
   expect_s3_class(x, "data.frame")
   expect_s3_class(y, "data.frame")
-  expect_s3_class(z, "data.frame")
   expect_s3_class(w, "data.frame")
 })
