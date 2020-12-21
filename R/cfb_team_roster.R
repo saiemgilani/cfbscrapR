d78e7a98a16a88737b9b5f884ce3f5bf79562e6c
Jared Lee <jaredlee@Jareds-MacBook-Pro.local>
2020-12-20 18:53:19 -0700

    Updated cfb_team_roster to allow no entry for team

--- R/cfb_team_roster.R
+++ R/cfb_team_roster.R
@@ -1,9 +1,10 @@
 #' Team Roster
 #' Get a teams full roster by year. If year not selected, API defaults to most recent year (2020 as of 9/22/20)
+#' If team is not selected, API returns rosters for every team from the selected year.
 #'
 #' @param team (\emph{String} required): Team, select a valid team in D-I football
 #' @param year (\emph{Integer} optional): Year,  4 digit format (\emph{YYYY})
-#' 
+#'
 #' @return A data frame with 12 variables:
 #' \describe{
 #'   \item{\code{athlete_id}}{character.}
@@ -29,11 +30,11 @@
 #' @importFrom glue glue
 #' @export
 #' @examples
-#' 
-#' cfb_team_roster("Florida State")
+#'
+#' cfb_team_roster(team = "Florida State")
 #'
 
-cfb_team_roster <- function(team, year = NULL){
+cfb_team_roster <- function(year = NULL,team = NULL){
   team2 <- team
   if(!is.null(year)){
     # check if year is numeric
@@ -51,9 +52,13 @@ cfb_team_roster <- function(team, year = NULL){
   }
   base_url <- "https://api.collegefootballdata.com/roster?"
 
-  full_url <- paste0(base_url,
-                     "team=", team,
-                     "&year=", year)
+  if(is.null(team)) {
+    full_url <- paste0(base_url,
+                       "year=", year)
+  } else {
+    full_url <- paste0(base_url,"team=", team,
+                       "&year=", year)
+  }
 
   # Check for internet
   check_internet()
@@ -63,16 +68,18 @@ cfb_team_roster <- function(team, year = NULL){
 
   # Check the result
   check_status(res)
-  
+
   df <- data.frame()
   tryCatch(
     expr ={
       # Get the content and return it as data.frame
-      df = jsonlite::fromJSON(full_url) %>% 
-        dplyr::rename(athlete_id = .data$id) %>% 
-        dplyr::mutate(team = team2) %>% 
+      df = jsonlite::fromJSON(full_url) %>%
+        dplyr::rename(athlete_id = .data$id) %>%
+        # Is this okay to just comment out?
+        # Changing to team = NULL deleted the column
+        #dplyr::mutate(team = team2) %>%
         as.data.frame()
-      
+
       message(glue::glue("{Sys.time()}: Scraping team roster..."))
     },
     error = function(e) {
--- tests/testthat/test-cfb_team_roster.R
+++ tests/testthat/test-cfb_team_roster.R
@@ -4,13 +4,17 @@ x <- cfb_team_roster(2019, team = 'Florida State')
 
 y <- cfb_team_roster(2018, team = 'Texas A&M')
 
-cols <- c("athlete_id", "first_name", "last_name", "weight", "height",
+z <- cfb_team_roster(2017)
+
+cols <- c("athlete_id", "first_name", "last_name","team", "weight", "height",
           "jersey", "year", "position","home_city","home_state",
-          "home_country","team")
+          "home_country")
 
 test_that("CFB Team Roster", {
   expect_equal(colnames(x), cols)
   expect_equal(colnames(y), cols)
+  expect_equal(colnames(z), cols)
   expect_s3_class(x, "data.frame")
   expect_s3_class(y, "data.frame")
+  expect_s3_class(z, "data.frame")
 })
